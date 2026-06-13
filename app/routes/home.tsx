e44239e7dae8b52f227e886669ef7957719237ff
Thijmen <thijmen.walter@hva.nl>
2026-06-13 13:42:08 +0200

    feat: :tada: Initial react router setup

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/home";
+import { HomePage } from "../views/homePage/homePage";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "WaltBoard" },
+    { name: "description", content: "Walter's kanban board." },
+  ];
+}
+
+export default function Home() {
+  return <HomePage />;
+}
---
+++ app/views/homePage/homePage.tsx
@@ -0,0 +1,9 @@
+
+export function HomePage() {
+  return (
+    <main className={"flex items-center justify-center"}>
+        <h1 className={"text-4xl m-10"}>Walter's kanban boards</h1>
+    </main>
+  );
+}
+
