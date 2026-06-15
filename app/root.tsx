4e4f555dc5fa9d30f9f269af73ccc3000e07530a
Thijmen <thijmen.walter@hva.nl>
2026-06-15 22:46:41 +0200

    feat: :sparkles: Add tanstack query

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,16 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import {
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
 import type { Route } from "./+types/root";
 import "./app.css";
+
+const queryClient = new QueryClient();
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +50,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+   <QueryClientProvider client={queryClient}>
+    <Outlet />
+   </QueryClientProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
